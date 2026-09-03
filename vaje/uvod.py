b65059dca63593aaa41c10767ae1a3a13d21fb35
Pfyber <luka.colaric@gmail.com>
2026-09-03 09:59:46 +0000

    2 vaja

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -6,5 +6,14 @@ def hello():
     else:
         print(f"Hello {oddelek} ")
 
+def poštevanka():
+    x = int(input("Izberi si število: "))
+    št = 1
+    while št <= 10:
+        print(f"{št} * {x} = {št * x}")
+        št += 1
+
 if __name__ == "__main__":
-    hello()
+    # To je moj prvi program
+    # hello()
+    poštevanka()
